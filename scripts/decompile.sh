d86417d5be7232061ab1002d53d7ad84c02a6447
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2025-10-16 04:40:24 +0800

    fix: replace Cpp2IL with Il2cppInspector.Redux
    * Due to incorrect field ordering in metadata v31

--- scripts/decompile.sh
+++ scripts/decompile.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-./.tools/Cpp2IL --force-binary-path ./libil2cpp.so \
-    --force-metadata-path global-metadata.dat --force-unity-version 31 \
-    --use-processor attributeinjector \
-    --output-as diffable-cs --output-to ./
+./.tools/Il2CppInspector.Redux.CLI -i ./libil2cpp.so -m global-metadata.dat \
+    -l tree -c DiffableCs \
+    --suppress-metadata --suppress-dll-metadata --select-outputs cs
 
 echo "Extracting protocolConverter VA offset value..."
 offset=$(rg -o --no-filename -B 2 -F "public int TypeConversion(uint crc, Protocol protocol)" ./DiffableCs/BlueArchive/MX/NetworkProtocol/ProtocolConverter.cs | rg -o -r '$1' 'Offset = "(0x[0-9A-Z]+)"')
