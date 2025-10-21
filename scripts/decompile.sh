c7072ecfd95d5f25b12359b09e910502a6b4bd42
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2025-10-21 23:58:32 +0800

    feat: remove metadata suppression

--- scripts/decompile.sh
+++ scripts/decompile.sh
@@ -2,7 +2,7 @@
 
 ./.tools/Il2CppInspector.Redux.CLI -i ./libil2cpp.so -m global-metadata.dat \
     -l tree -c DiffableCs \
-    --suppress-metadata --suppress-dll-metadata --select-outputs cs
+    --suppress-dll-metadata --select-outputs cs
 
 echo "Extracting protocolConverter VA offset value..."
 offset=$(rg "^\s+public int TypeConversion\(uint crc, Protocol protocol\);.*// (0x[0-9A-F]+)-" -o --no-filename -r '$1' ./DiffableCs/BlueArchive/MX/NetworkProtocol/ProtocolConverter.cs)
