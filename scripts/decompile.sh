38894fa8c9839b46f736cbe1bb200225bd3c0ee9
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2026-08-23 07:33:41 +0800

    fix(ci): use legacy cli binary for export

--- scripts/decompile.sh
+++ scripts/decompile.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-./.tools/Il2CppInspector.Redux.CLI process libil2cpp.so global-metadata.dat \
-    -s --layout tree --suppress-dll-metadata
+./.tools/Il2CppInspector -i ./libil2cpp.so \
+    -m ./global-metadata.dat \
+    -l tree \
+    -c DiffableCs \
+    --suppress-dll-metadata \
+    --select-outputs cs
 
 echo "Extracting protocolConverter VA offset value..."
-offset=$(rg "^\s+public int TypeConversion\(uint crc, Protocol protocol\);.*// (0x[0-9A-F]+)-" -o --no-filename -r '$1' ./cs/BlueArchive/Mx/NetworkProtocol/ProtocolConverter.cs)
+offset=$(rg "^\s+public int TypeConversion\(uint crc, Protocol protocol\);.*// (0x[0-9A-F]+)-" -o --no-filename -r '$1' ./DiffableCs/BlueArchive/MX/NetworkProtocol/ProtocolConverter.cs)
 echo "::notice title=VA Offset::$offset"
 echo "offset=$offset" >> $GITHUB_OUTPUT
 echo "Offset extracted."
