ebf4cd994aa61a641d94e950ac3f721eb44029b1
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2026-08-23 07:13:55 +0800

    fix: correct command syntax in decompile script

--- scripts/decompile.sh
+++ scripts/decompile.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-./.tools/Il2CppInspector.Redux.CLI -i ./libil2cpp.so -m global-metadata.dat \
-    -l tree -c DiffableCs \
-    --suppress-dll-metadata --select-outputs cs
+./.tools/Il2CppInspector.Redux.CLI process libil2cpp.so global-metadata.dat \
+    -s --layout tree --suppress-dll-metadata
 
 echo "Extracting protocolConverter VA offset value..."
-offset=$(rg "^\s+public int TypeConversion\(uint crc, Protocol protocol\);.*// (0x[0-9A-F]+)-" -o --no-filename -r '$1' ./DiffableCs/BlueArchive/Mx/NetworkProtocol/ProtocolConverter.cs)
+offset=$(rg "^\s+public int TypeConversion\(uint crc, Protocol protocol\);.*// (0x[0-9A-F]+)-" -o --no-filename -r '$1' ./cs/BlueArchive/Mx/NetworkProtocol/ProtocolConverter.cs)
 echo "::notice title=VA Offset::$offset"
 echo "offset=$offset" >> $GITHUB_OUTPUT
 echo "Offset extracted."
